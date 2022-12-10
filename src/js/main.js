33297db373ff258e076851840672f5e03da3241e
Maksym Pecherytsia <findimrk@gmail.com>
2022-12-10 22:03:23 +0200

    add module showInfo and download function

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import VideoPlayer from "./modules/playVideo";
 import MiniSlider from "./modules/slider/slider-mini.js";
 import Difference from "./modules/difference";
 import Form from "./modules/form";
+import ShowInfo from "./modules/showInfo";
+import Download from "./modules/download";
 
 window.addEventListener("DOMContentLoaded", () => {
 	const slider = new MainSlider({ btns: ".next", container: ".page" });
@@ -43,9 +45,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
 	new VideoPlayer(".showup .play", ".overlay").init();
 	new VideoPlayer(".module__video-item .play", ".overlay").init();
-	
 
 	new Difference(".officerold", ".officernew", ".officer__card-item").init();
 
 	new Form(".form").init();
+
+	new ShowInfo(".plus__content").init();
+
+	new Download(".download").init();
 });
